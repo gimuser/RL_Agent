0915b18630b86097dd4440c9e723a79316959539
gimuser <oualidaitalla18@gmail.com>
2026-08-03 17:40:53 +0100

    Update organisation

--- data/data_getter.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Download Kaggle dataset train/test files using Python and kagglehub.
-REM Usage:
-REM   python download_kaggle_dataset.py
-REM   python download_kaggle_dataset.py --dataset avijitjana101/microsoft-soc-dataset
-REM   python download_kaggle_dataset.py --output-dir ..\data
-
-SET PYTHON=python
-%PYTHON% download_kaggle_dataset.py %*
