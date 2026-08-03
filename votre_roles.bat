faca33e57897e723c3200e8502d6b042972fccb0
gimuser <oualidaitalla18@gmail.com>
2026-08-03 20:03:31 +0100

    Update organisation

--- votre_roles.bat
+++ votre_roles.bat
@@ -1,3 +1,12 @@
 @echo off
-start "" "%~dp0docs\team_structure.html"
-start "" "%~dp0docs\branch_responsibilities.html"
+set "SCRIPT_DIR=%~dp0"
+if not exist "%SCRIPT_DIR%docs\team_structure.html" (
+  echo Error: docs\team_structure.html not found in %SCRIPT_DIR%
+  exit /b 1
+)
+if not exist "%SCRIPT_DIR%docs\branch_responsibilities.html" (
+  echo Error: docs\branch_responsibilities.html not found in %SCRIPT_DIR%
+  exit /b 1
+)
+start "" "%SCRIPT_DIR%docs\team_structure.html"
+start "" "%SCRIPT_DIR%docs\branch_responsibilities.html"
